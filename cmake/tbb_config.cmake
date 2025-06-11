4c6c8ce2336dae604b8ffe3950c795fec11475ff
moldenha <smoldenha@gmail.com>
2025-06-11 00:15:43 -0400

    Added workflow to archive mac-os intel, removed tbbmalloc dependency in cmake

--- cmake/tbb_config.cmake
+++ cmake/tbb_config.cmake
@@ -17,6 +17,8 @@ else()
     # If TBB is not found, build it from the third-party directory
     message(STATUS "System-installed TBB not found, building from source")
     set(TBB_TEST OFF CACHE BOOL "Disable TBB tests") # Must be above add_subdirectory to take effect
+    set(TBB_BUILD_TBBMALLOC OFF CACHE BOOL "" FORCE)
+    set(TBB_BUILD_TBBMALLOC_PROXY OFF CACHE BOOL "" FORCE)
     add_subdirectory(third_party/tbb)
     set(TBB_LIB tbb)  # Use the built TBB target, when built from source just called tbb
     include_directories(${CMAKE_SOURCE_DIR}/third_party/tbb/include)
@@ -25,6 +27,10 @@ else()
     # If TBB is not found, build it from the third-party directory
     message(STATUS "building TBB from source")
     set(TBB_TEST OFF CACHE BOOL "Disable TBB tests") # Must be above add_subdirectory to take effect
+    set(TBB_BUILD_TBBMALLOC OFF CACHE BOOL "" FORCE)
+    set(TBB_BUILD_TBBMALLOC_PROXY OFF CACHE BOOL "" FORCE)
+    set(TBB_BUILD_STATIC ON CACHE BOOL "Enable static build of TBB" FORCE)
+    set(TBB_BUILD_SHARED OFF CACHE BOOL "Disable shared build of TBB" FORCE)
     add_subdirectory(third_party/tbb)
     set(TBB_LIB tbb)  # Use the built TBB target, when built from source just called tbb
     include_directories(${CMAKE_SOURCE_DIR}/third_party/tbb/include)
