15015f6528d3c5def2aba04a439270e23bb82d44
moldenha <smoldenha@gmail.com>
2025-06-11 12:09:31 -0400

    Added flags to make tbb compile statically

--- cmake/tbb_config.cmake
+++ cmake/tbb_config.cmake
@@ -21,7 +21,8 @@ else()
     # set(TBB_BUILD_TBBMALLOC_PROXY OFF CACHE BOOL "" FORCE)
     set(BUILD_SHARED_LIBS OFF CACHE BOOL "Disable shared build of TBB" FORCE)
     set(TBB_INSTALL_DIR_SKIP ON CACHE BOOL "Skip TBB install" FORCE) # No reason to install TBB
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D__TBB_DYNAMIC_LOAD_ENABLED=0") #ensure dl is disabled for fully static build
+    add_compile_definitions(__TBB_DYNAMIC_LOAD_ENABLED=0)
+    set(TBB_ENABLE_IPO OFF CACHE BOOL "Disable IPO to compile tbb statically")
     add_subdirectory(third_party/tbb)
     set(TBB_LIB tbb tbbmalloc)  # Use the built TBB target, when built from source just called tbb
     include_directories(${CMAKE_SOURCE_DIR}/third_party/tbb/include)
@@ -35,7 +36,8 @@ else()
     set(BUILD_SHARED_LIBS OFF CACHE BOOL "Disable shared build of TBB" FORCE)
     set(TBB_INSTALL_DIR_SKIP ON CACHE BOOL "Skip TBB install" FORCE) # No reason to install TBB
     add_compile_definitions(_SILENCE_CXX17_C_HEADER_DEPRECATION_WARNING)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D__TBB_DYNAMIC_LOAD_ENABLED=0") #ensure dl is disabled for fully static build
+    add_compile_definitions(__TBB_DYNAMIC_LOAD_ENABLED=0)
+    set(TBB_ENABLE_IPO OFF CACHE BOOL "Disable IPO to compile tbb statically")
     add_subdirectory(third_party/tbb)
     set(TBB_LIB tbb tbbmalloc)  # Use the built TBB target, when built from source just called tbb
     include_directories(${CMAKE_SOURCE_DIR}/third_party/tbb/include)
